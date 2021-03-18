5d7b31bd56e5c274ee56286d558cfa9aeb5bded0
GtlsGamr <hittarth91@gmail.com>
2021-03-18 14:55:45 +0530

    add

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include "layouts.c"
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -10,25 +10,25 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF UI Display:size=10:style=Regular","NotoEmoji:size=10:antialias=true:autohint=true"};
-static const char dmenufont[]       = "SF UI Display:size=10";
+static const char *fonts[]          = { "Product Sans:size=14:style=Regular","NotoEmoji:size=12:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Product Sans:size=14";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#332d39";
-static const char col_border[]      = "#a848b5";
+static const char col_border[]      = "#fff";
 static const unsigned int baralpha = 200;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_border  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, baralpha },
-	[SchemeSel]  = { OPAQUE, baralpha, baralpha },
+	[SchemeNorm] = { OPAQUE, 90, baralpha },
+	[SchemeSel]  = { OPAQUE, 90, OPAQUE},
 };
 
 /* tagging */
@@ -82,7 +82,7 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioLowerVolume,	spawn,				SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_MonBrightnessUp,	spawn,				SHCMD("xbacklight -inc 5; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_MonBrightnessDown,	spawn,				SHCMD("xbacklight -dec 5; kill -44 $(pidof dwmblocks)") },
-	{ MODKEY,			XK_w,	   spawn,		SHCMD("firefox") },
+	{ MODKEY,			XK_w,	   spawn,		SHCMD("brave --force-device-scale-factor=1.25") },
 	{ MODKEY,			XK_r, 	   spawn, 		SHCMD("st -e lf")},
 	{ MODKEY,             		XK_d, 	   spawn,	      	{.v = dmenucmd } },
 	{ MODKEY,             		XK_Return, spawn,          	{.v = termcmd } },
