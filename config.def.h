e04d6fb40bcce967110945f1c7dc98bd925e6736
GtlsGamr <hittarth91@gmail.com>
2021-07-27 23:28:09 +0530

    light theme added

--- config.def.h
+++ config.def.h
@@ -1,13 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {  "JetBrainsMonoMedium Nerd Font Mono:size=12","NotoEmoji:size=12:antialias=true:autohint=true","JetBrainsMonoMedium Nerd Font Mono:pixelsize=12:antialias=true:autohint=true"};
-static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font Mono:size=12";
+static const char *fonts[]          = {  "SF Pro Rounded:size=14","NotoEmoji:size=14:antialias=true:autohint=true","SF Pro Rounded:pixelsize=14:antialias=true:autohint=true"};
+static const char dmenufont[]       = "SF Pro Rounded:size=14";
 
+static const char col_macos_white[]	= "#f5f5f5";
+static const char col_macos_gray[]	= "#C0BFC0";
 static const char col_gray4[]       = "#fdf1c7";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray2[]       = "#3c3836";
@@ -15,8 +17,8 @@ static const char col_gray1[]       = "#080808";
 static const char col_cyan[]        = "#7c8f8f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray1, col_macos_white,  col_macos_gray },
+	[SchemeSel]  = { col_gray1, col_macos_white,  col_macos_gray  },
 };
 
 /* tagging */
@@ -58,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_macos_white, "-nf", col_gray1, "-sb", col_macos_gray, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
