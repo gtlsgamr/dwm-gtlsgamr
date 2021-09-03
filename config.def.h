b96a5b5fc69780a2bb3c06f015df3051a435455b
Hitarth Thummar <47787284+gtlsgamr@users.noreply.github.com>
2021-09-03 16:47:38 +0530

    added anybar patch

--- config.def.h
+++ config.def.h
@@ -5,6 +5,10 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *alttrayname      = "tray";    /* Polybar tray instance name */
+static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = {  "SF Pro Display:style=SemiBold:size=14","NotoEmoji:size=14:antialias=true:autohint=true"};
 static const char dmenufont[]       = "SF Pro Display:style=SemiBold:size=14";
 
