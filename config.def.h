949e0a89611588aaefa3528b3c3be191b25c1066
Hitarth Thummar <hitarth.thummar@protonmail.com>
2022-02-08 15:15:43 +0530

    full look overhaul, added vacant tags patch

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 1;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 5;        /* vertical padding for statusbar */
-static const char *fonts[]          = {  "W95FA:style=Bold:size=16","Material Icons:style=Regular:size=12"};
-static const char dmenufont[]       = "W95FA:style=Bold:size=16";
+static const char *fonts[]          = {  "SF Pro Display:style=Regular:size=14","Material Icons:style=Regular:size=12"};
+static const char dmenufont[]       = "SF Pro Display:style=Regular:size=14";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char col_win95_fg[]	= "#000000";
@@ -23,8 +23,8 @@ static const char col_cyan[]        = "#7c8f8f";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_win95_fg, col_gray3,  col_gray1 },
-	[SchemeSel]  = { col_win95_selected_text, col_gray2,  col_gray2  },
+	[SchemeNorm] = { col_win95_bg, col_gray1,  col_gray1 },
+	[SchemeSel]  = { col_win95_selected_text, col_gray1,  col_gray2  },
 };
 
 static const unsigned int alphas[][3]      = {
@@ -34,7 +34,7 @@ static const unsigned int alphas[][3]      = {
 };
  
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -53,9 +53,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -71,7 +71,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray3, "-nf", col_win95_fg, "-sb", col_gray2, "-sf", col_win95_selected_text, NULL };
+static const char *dmenucmd[] = {
+	"dmenu_run",
+	"-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", col_gray1,
+	"-nf", col_win95_bg,
+	"-sb", col_gray1,
+	"-sf", col_win95_selected_text,
+	NULL
+};
 static const char quicknotecommand[] = "touch ~/Documents/notes/scratchpad.md && st -t centersmall -e nvim ~/Documents/notes/scratchpad.md";
 static const char *termcmd[]  = { "st", NULL };
 
